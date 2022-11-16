7bbbc75119898dc0614b007d60e1780f4b585182
Matheus Barcelos <barcelosmatheus97@gmail.com>
2022-11-16 19:08:58 -0300

    Configuração banco de dados H2

--- src/main/java/com/grupo03/petshop/resources/CategoriaResource.java
+++ src/main/java/com/grupo03/petshop/resources/CategoriaResource.java
@@ -17,8 +17,8 @@ public class CategoriaResource {
 	public List<Categoria>  mostrar() {
 		
 		Categoria cat1 = new Categoria(1, "Alimento"); 
-		Categoria cat2 = new Categoria(1, "Cosmético"); 
-		Categoria cat3 = new Categoria(1, "Remédio"); 
+		Categoria cat2 = new Categoria(2, "Cosmético"); 
+		Categoria cat3 = new Categoria(3, "Remédio"); 
 		
 		List<Categoria> lista = new ArrayList<>();
 		
