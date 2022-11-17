6df9da4e84341692b336b201491b8be0a5b88283
anajudith <ana-judth@hotmail.com>
2022-11-17 18:31:41 -0300

    Popula dados Produto e Categoria, associação muitos para muitos

---
+++ src/main/java/com/grupo03/petshop/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.grupo03.petshop.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.grupo03.petshop.domain.Produto;
+
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto,Integer>{
+
+}
